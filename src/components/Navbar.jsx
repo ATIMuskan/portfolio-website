a8ca6eeb8a20dcc3cd956862b45ceca3511909ff
muskan shrivas <mknv05514@gmail.com>
2025-05-23 00:41:03 +0530

    Update Navbar.jsx
    added hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,12 @@
-import React, { useState, useEffect } from 'react'; // Make sure to import useState and useEffect
+import  { useState, useEffect } from 'react';
 import './Navbar.css';
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [scrolling, setScrolling] = useState(false);
-
+ 
   const toggleMenu = () => setMenuOpen(!menuOpen);
 
-  // Check if the user has scrolled down
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 50) {
@@ -27,19 +26,17 @@ function Navbar() {
   return (
     <nav className={`navbar ${scrolling ? 'scrolled' : ''}`}>
       <div className="navbar-container">
-        {/* Logo Section */}
         <div className="logo">
           <a href="#home" className="logo-link">
             <span className="logo-tech">Learn</span> Tech with Muskan
           </a>
         </div>
 
-        {/* Mobile Menu Toggle */}
-        <div className="menu-icon" onClick={toggleMenu}>
-          <span className={menuOpen ? 'icon-close' : 'icon-menu'}></span>
-        </div>
+       <div className="menu-icon" onClick={toggleMenu}>
+  {menuOpen ? '✖' : '☰'}
+</div>
 
-        {/* Navbar Links */}
+
         <ul className={`navbar-links ${menuOpen ? 'active' : ''}`}>
           <li><a href="#home" className="nav-link">Home</a></li>
           <li><a href="#about" className="nav-link">About</a></li>
